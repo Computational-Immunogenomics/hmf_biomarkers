65cd66892609db5ce63051f54b9806d1ce71f479
Joseph Usset <josephusset@vhiopc220818b.vhio.org>
2024-07-19 16:07:34 +0200

    everything

---
+++ .ipynb_checkpoints/map-checkpoint.r
@@ -0,0 +1,20 @@
+I_DIR = "/mnt/petasan_immunocomp/datasets/hartwig/"
+O_DIR = "/mnt/petasan_immunocomp/datasets/hartwig/biomarkers/"
+TMP_DIR = "/mnt/petasan_immunocomp/datasets/hartwig/biomarkers/tmp/"
+READY_DIR= "/mnt/petasan_immunocomp/datasets/hartwig/biomarkers/ready/"
+
+### File path mapper for samples
+get_fp <- function(i, type = "purity"){
+  if( type == "purity") { as.character(paste0(I_DIR, 'somatic/', i, "/purple/", i, ".purple.purity.tsv")) }
+  else if (type == "drivers") { as.character(paste0(I_DIR, 'somatic/', i, "/linx/", i, ".linx.driver.catalog.tsv")) }
+  else if (type == "cnv_gene") { as.character(paste0(I_DIR, 'somatic/', i, "/purple/", i, ".purple.cnv.gene.tsv")) }
+  else if (type == "isofox"){ as.character(paste0(I_DIR, 'isofox/data_isofox/', i, "/", i, ".isf.gene_data.csv")) }
+  else if (type == "lilac"){ as.character(paste0(I_DIR, 'lilac/lilac_out/', i, ".lilac.tsv")) }
+  else if (type == "lilac_qc"){ as.character(paste0(I_DIR, 'lilac/lilac_out/', i, ".lilac.qc.tsv")) }
+  else if (type == "teal"){ as.character(paste0(I_DIR, 'teal/', i, ".teal.tellength.tsv")) }
+  else if (type == "neo"){ as.character(paste0(I_DIR, 'neo/data_neo/', i, "/", i, ".neo.neoepitope.tsv.gz")) }
+  else if (type == "neo_pep"){ as.character(paste0(I_DIR, 'neo/data_neo/', i,"/", i, ".neo.peptide_scores.tsv.gz")) }
+}
+reader <- function( i_file = "file_path", sample = "ACTN01020001T"){
+    fread( i_file ) %>% mutate(sampleId = sample) 
+}
---
+++ 0_gather_clinical/helpers/clinical_help.r
@@ -0,0 +1,19 @@
+sex <- function(gender){
+  if(!is.na(gender)){
+    if( tolower(gender) == "female"){ 1 }
+    else { 0 }
+  } else{ NA }
+}
+age <- function( birthYear, biopsyDate ){
+  if(!is.na(birthYear) & !is.na(biopsyDate)){
+    biopsyYear <- as.numeric(strsplit(biopsyDate, "-")[[1]][1])
+    age <- biopsyYear - as.numeric(birthYear)
+  } else { NA }
+}
+responder <- function(v){
+  if( "CR" %in% v || "PR" %in% v ){ 1 }
+  else{ 0 }
+}
+trt_indicator <- function( v ){
+    apply(data.frame(lapply( v, function(i) grepl(i, base$treatment))), 1, sum)
+}
