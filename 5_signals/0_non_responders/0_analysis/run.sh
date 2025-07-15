b86928103e01d9fc19ede9a27b2a63d4a86f8168
Joseph Usset <josephusset@vhiopc220818b.vhio.org>
2025-07-15 16:47:19 +0200

    check in

--- 5_signals/0_non_responders/0_analysis/run.sh
+++ 5_signals/0_non_responders/0_analysis/run.sh
@@ -2,5 +2,5 @@ jupyter nbconvert --to script *.ipynb
 echo "Prep features"; Rscript 0_prep_features.r;
 echo "Prep cohorts"; Rscript 1_prep_cohorts.r;
 echo "Run marginal tests"; Rscript 2_run_marginal.r;
-echo "Run combination tests"; Rscript 3_run_interaction.r;
+echo "Run combination tests"; Rscript 3_ready_interaction.r;
 echo "Run simulation .."; Rscript 4_simulation.r;
