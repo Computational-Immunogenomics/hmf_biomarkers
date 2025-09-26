48c1c755093ef42d6108d1c4f7f69b905af3f9d5
Joseph Usset <josephusset@vhiopc220818b.vhio.org>
2025-09-26 11:53:23 +0200

    remove analysis

--- 3_signals/0_analysis/run.sh
+++
@@ -1,5 +0,0 @@
-jupyter nbconvert --to script *.ipynb
-echo "prep features"; Rscript 0_prep_features.r;
-echo "prep cohorts"; Rscript 1_prep_cohorts.r;
-echo "run tests"; Rscript 2_run.r;
-echo "prep results"; Rscript 3_ready.r;
